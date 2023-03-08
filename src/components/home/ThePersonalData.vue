0561ade9202b711da7efe11ca167d2fa15a65b70
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-07 21:12:41 -0500

    creating ThePersonalData file code and creating variables with sass

--- src/components/home/ThePersonalData.vue
+++ src/components/home/ThePersonalData.vue
@@ -1,12 +1,31 @@
 <template>
-    <div>
-        <h3>Here are personal data about the person</h3>
+    <div class="personal-data">
+        <div class="personal-data__left">
+            <span class="personal-data__left__text">Hola</span>
+            <h3 class="personal-data__left__name">Soy Goku</h3>
+            <p class="personal-data__left__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+                eligendi quibusdam voluptate non maxime
+                natus consectetur.</p>
+
+            <!--Import base component VButtonMain -->
+            <v-button-main texto="Contactar" />
+        </div>
+
+        <div class="personal-data__right">
+            <img src="@/assets/goku.png">
+        </div>
     </div>
 </template>
 
 <script>
+
+import VButtonMain from '@/components/base/VButtonMain'
+
 export default {
-    name: 'ThePersonalData'
+    name: 'ThePersonalData',
+    components: {
+        VButtonMain
+    }
 }
 </script>
 
