f9b3794b308d5b4ce904d008781b22ccf0248836
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-08 21:22:31 -0500

    finish third block

--- src/components/base/VCardMain.vue
+++ src/components/base/VCardMain.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-card" :style="{ background: color }">
-        <h4>{{ titulo }}</h4>
-        <p>{{ contenido }}</p>
+        <h4 class="main-card__title">{{ title }}</h4>
+        <p class="main-card__text">{{ content }}</p>
     </div>
 </template>
 
@@ -9,11 +9,31 @@
 export default {
     name: 'VCardMain',
     props: {
-        titulo: String,
-        contenido: String,
+        title: String,
+        content: String,
         color: String,
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/components/app.scss';
+
+.main-card {
+    width: 300px;
+    padding: 2em;
+    color: $color-white;
+    margin: 10px;
+    height: 150px;
+
+    &__title {
+        font-family: $ff-primary;
+        font-size: 20px;
+        margin-bottom: 1em;
+    }
+
+    &__text {
+        font-family: $ff-secondary;
+    }
+}
+</style>
--- src/components/home/TheSkills.vue
+++ src/components/home/TheSkills.vue
@@ -1,13 +1,74 @@
 <template>
-    <div>
-        <h3>Here are the skills about the person</h3>
+    <div class="skills">
+        <h3 class="skills__title">Mis habilidades</h3>
+
+        <p class="skills__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, totam. Dolore eligendi
+            ipsam iste dolorem
+            laudantium
+            accusantium.</p>
+
+        <div class="skills__items">
+            <div v-for="skill in skills" :key="skill.id" class="skills__item">
+                <v-card-main :title="skill.title" :content="skill.content" :color="skill.color" />
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+
+import VCardMain from '@/components/base/VCardMain'
+
 export default {
-    name: 'TheSkills'
+    name: 'TheSkills',
+    data() {
+        return {
+            skills: [
+                { 'id': 0, 'title': 'Fuerza', 'content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit', color: '#ff9f1a' },
+
+                { 'id': 1, 'title': 'Velocidad', 'content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit', color: '#18dcff' },
+
+                { 'id': 2, 'title': 'Teletransportación', 'content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit', color: '#c56cf0' },
+
+                { 'id': 3, 'title': 'UltraInstinto', 'content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit', color: '#3ae374' }
+            ]
+        }
+    },
+    components: {
+        VCardMain
+    }
+
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/components/app.scss';
+
+.skills {
+    background: $color1;
+    padding: 4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    &__title {
+        font-family: $ff-primary;
+        color: $color-white;
+        margin-bottom: 1em;
+        font-size: 24px;
+    }
+
+    &__text {
+        font-family: $ff-secondary;
+        color: $color-white;
+        margin-bottom: 1em;
+    }
+
+    &__items {
+        display: grid;
+        grid-template-columns: 400px 400px;
+    }
+}
+</style>
