b7fd4a24613e3d71319ff973484b16920d48bce4
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-08 20:44:18 -0500

    create base component VCardMain

---
+++ src/components/base/VCardMain.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-card" :style="{ background: color }">
+        <h4>{{ titulo }}</h4>
+        <p>{{ contenido }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VCardMain',
+    props: {
+        titulo: String,
+        contenido: String,
+        color: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
